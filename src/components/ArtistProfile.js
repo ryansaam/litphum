2666007649f38b6686fc6d0203101db4910df89e
Ryan Sam <ryansam.inquiries@gmail.com>
2019-03-09 17:57:53 -0600

    Added artist and album links to the ArtistProfile component

--- src/components/ArtistProfile.js
+++ src/components/ArtistProfile.js
@@ -1,6 +1,13 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { useAsync } from "react-async"
+import { Link } from "react-router-dom"
 import styled from 'styled-components'
+
+const PlayBtn = props => (
+  <svg style={{visibility: props.visibility ? "visible" : "hidden" }} fill="white" width="60" height="60" xmlns="http://www.w3.org/2000/svg">
+    <path id="control-play" d="m40,30c0,0.34 -0.173,0.657 -0.459,0.841l-10.01,6.435c-0.466,0.298 -1.083,0.164 -1.382,-0.299c-0.298,-0.465 -0.164,-1.083 0.3,-1.382l8.702,-5.595l-11.151,-7.168l0,16.168c0,0.553 -0.448,1 -1,1c-0.552,0 -1,-0.447 -1,-1l0,-18c0,-0.366 0.2,-0.702 0.521,-0.878c0.32,-0.175 0.711,-0.163 1.02,0.037l14,9c0.286,0.184 0.459,0.501 0.459,0.841m-10,28c-15.439,0 -28,-12.561 -28,-28c0,-15.439 12.561,-28 28,-28c15.439,0 28,12.561 28,28c0,15.439 -12.561,28 -28,28m0,-58c-16.542,0 -30,13.458 -30,30c0,16.542 13.458,30 30,30c16.542,0 30,-13.458 30,-30c0,-16.542 -13.458,-30 -30,-30"/>
+  </svg>
+)
 
 const ArtistProfileContainer = styled.div`
   background-color: #1d1d1d;
@@ -40,6 +47,16 @@ const PlayButtonContainer = styled.div`
   fill: #fff;
   stroke: none;
   padding: 0px 15px;
+`
+const ImageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  :hover {
+    background-color: rgba(0,0,0,0.6)
+  }
 `
 
 const PlayButton = () => (
@@ -101,20 +118,38 @@ const TextOverflow = styled.div`
   text-overflow: ellipsis;
   overflow: hidden;
 `
+const Underline = styled.span`
+  :hover {
+    text-decoration: underline
+  }
+`
 const AlbumTag = props => {
-  const artistsNames = props.artists.map(artist => {
-    return artist.name
-  });
+  const [bool, setBool] = useState(false)
   return (
     <div style={{marginBottom: "20px"}}>
-      <TagImage size={200} imageMargin={"auto"} image={props.image} />
+      <Link to={"/album/"+props.albumId} >
+        <TagImage size={200} imageMargin={"auto"} image={props.image}>
+         <ImageContainer onMouseEnter={() => setBool(true)} onMouseLeave={() => setBool(false)} >
+            <PlayBtn visibility={bool} />
+         </ImageContainer>
+        </TagImage>
+      </Link>
       <AlbumInfo>
-        <TextOverflow>
-          <div style={{display: "inline"}}>
-            <span>{props.name}</span>
-          </div>
-        </TextOverflow>
-        <span style={{opacity: "0.7", margin: "5px 0px", display: "block"}}>{artistsNames.toString()}</span>
+        <Link style={{color: "white"}} to={"/album/"+props.albumId} >
+          <TextOverflow>
+            <div style={{display: "inline"}}>
+              <span>{props.name}</span>
+            </div>
+          </TextOverflow>
+        </Link>
+        <Underline>
+          <Link to={"/artist/"+props.artistId} style={{
+            color: "white",
+            opacity: "0.7",
+            margin: "5px 0px",
+            display: "block"
+          }}>{props.artistName}</Link>
+        </Underline>
       </AlbumInfo>
     </div>
   )
@@ -201,8 +236,10 @@ const  ArtistProfile = props => {
               <AlbumTag
                 image={album.images[0].url}
                 name={album.name}
-                artists={album.artists}
+                artistName={data.artist.name}
+                artistId={id}
                 key={album.id}
+                albumId={album.id}
               />
             )
           })
